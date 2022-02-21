cf2c020fb07e70c8f33240aafea32063317f88ea
luckson1 <jacksongathondu@gmail.com>
2022-02-21 23:56:51 +0300

    User Authentication
    Creating user login and authentication

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,55 +1,63 @@
-const {buildSchema} =require('graphql')
+const { buildSchema } = require('graphql');
 
-module.exports=buildSchema(`
+module.exports = buildSchema(`
 type Booking {
     _id: ID!
     event: Event!
     user: User!
     createdAt: String!
-    UpdtedAt:String!
+    updatedAt: String!
 }
+
 type Event {
-    _id: ID!
-    title:String!
-    description:String!
-    price:Float!
-    date: String!
-    creator: User!
+  _id: ID!
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+  creator: User!
 }
 
 type User {
-    _id: ID!
-    email:String!
-    password:String
-    CreatedEvent: Event!
+  _id: ID!
+  email: String!
+  password: String
+  createdEvents: [Event!]
 }
-input EventInput {
-    title:String!
-    description:String!
-    price:Float!
-    date: String!
 
+type AuthData {
+  userId: ID!
+  token: String!
+  tokenExpiration: Int!
+}
+
+input EventInput {
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
 }
 
 input UserInput {
-    email: String!
-    password: String!
+  email: String!
+  password: String!
 }
+
 type RootQuery {
     events: [Event!]!
     bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
 }
 
 type RootMutation {
-    createEvent(eventInput:EventInput): Event
+    createEvent(eventInput: EventInput): Event
     createUser(userInput: UserInput): User
-    bookEvent(eventId:ID!):Booking!
-    CancelBooking(bookingId:ID!):Event!
+    bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
 }
-
 
 schema {
     query: RootQuery
     mutation: RootMutation
 }
-`)
+`);
